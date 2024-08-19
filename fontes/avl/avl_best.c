ef57504be9334184121f980ba221e9de19432e20
Marlison Silva <marlisonsoaresjp@gmail.com>
2024-08-19 17:09:23 -0300

    best binario e outras imagens

--- fontes/avl/avl_best.c
+++ fontes/avl/avl_best.c
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
     srand(time(NULL));
 
     for (int i = 0; i < n; i++) {
-        btinsert(&r, new_node(rand() % (n*10)));
+        btinsert(&r, new_node(i));
     }
 
     // Inicializando variáveis de tempo
